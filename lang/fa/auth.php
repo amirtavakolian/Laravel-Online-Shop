93730274ba2afb565fed3f32893770a302c20896
Amir Tavakolian <rezatva@gmail.com>
2024-11-07 14:59:18 +0330

    add fa & en validation messages

---
+++ lang/fa/auth.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'mobile_number_empty' => 'فیلد شماره موبایل نباید خالی باشد',
+];
---
+++ lang/fa/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'auth' => [
+        'enterance_code_is_sent' => 'کد ورود برای شما ارسال شد',
+        'your_login_code' => 'کد ورود به سایت :code',
+        'otp_is_currently_generated' =>'کد ورود قبلا برای شما ارسال شده است',
+    ]
+];
