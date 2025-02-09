f9b75730aa7d61429fbb1541a6c42497b27b1a70
Amir Tavakolian <rezatva@gmail.com>
2025-02-09 23:08:35 +0330

    feat: get all permissions

--- app/Http/Controllers/RolePermission/PermissionController.php
+++ app/Http/Controllers/RolePermission/PermissionController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\RolePermission;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RolePermission\StorePermissionsRequest;
+use App\Http\Resources\RolePermission\PermissionResource;
 use App\Services\ApiResponse\ApiResponseFacade;
+use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class PermissionController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        $this->authorize('create', Role::class);
+
+        $permissions = PermissionResource::collection(Permission::all());
+
+        return ApiResponseFacade::setData($permissions->toArray($request))->build()->response();
+    }
 
     public function store(StorePermissionsRequest $request)
     {
