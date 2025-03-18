7cb0672706355d707eafafe9bd3d8a1049bded06
Amir Tavakolian <rezatva@gmail.com>
2025-03-18 12:42:28 +0330

    refactor: ticket service

--- app/Services/Ticket/TicketService.php
+++ app/Services/Ticket/TicketService.php
@@ -83,13 +83,13 @@ class TicketService
 
         $toCoworker = Coworker::find($request->input('to_coworker'));
 
-        if (is_null($ticketToAssign->opened_by) || $ticketToAssign->opened_by != $fromCoworker->id) {
-            return __('messages.tickets.you_cant_assign_the_ticket_which_you_dont_open');
-        }
+        $result = $this->isTicketOpenedByCurrentUser($ticketToAssign, $fromCoworker, 'you_cant_assign_the_ticket_which_you_dont_open');
 
-        if (!in_array($ticketToAssign->support_department_id, $toCoworker->supportDepartments->pluck('id')->toArray())) {
-            return __('messages.tickets.you_cant_assign_the_ticket_to_coworkers_of_other_departments');
-        }
+        if (is_string($result)) return $result;
+
+        $result = $this->isTicketBelongsToUserDepartment($ticketToAssign, $toCoworker, 'you_cant_assign_the_ticket_to_coworkers_of_other_departments');
+
+        if (is_string($result)) return $result;
 
         $assigneeOpenTicketsCount = Ticket::query()->where('opened_by', $request->input('to_coworker'))
             ->whereNull('closed_at')->count();
@@ -106,9 +106,7 @@ class TicketService
             'assign_reason' => $request->input('assign_reason'),
         ]);
 
-        $ticketToAssign->update([
-            'opened_by' => $toCoworker->id
-        ]);
+        $ticketToAssign->update(['opened_by' => $toCoworker->id]);
 
         $admins = Coworker::query()->whereHas('roles', function ($q) {
             $q->where('name', Role::SUPER_ADMIN->value);
@@ -116,11 +114,25 @@ class TicketService
 
         $toCoworker->notify(new AssignTicketNotification($fromCoworker, $ticketToAssign));
 
-        foreach ($admins as $admin) {
+        $admins->each(function ($admin) use ($fromCoworker, $toCoworker, $ticketToAssign) {
             $admin->notify(new AssignTicketToCoworkerAlertNotification($fromCoworker, $toCoworker, $ticketToAssign));
-        }
+        });
 
         return true;
+    }
+
+    private function isTicketOpenedByCurrentUser($ticket, $fromCoworker, $message)
+    {
+        if (is_null($ticket->opened_by) || $ticket->opened_by != $fromCoworker->id) {
+            return __('messages.tickets.' . $message);
+        }
+    }
+
+    private function isTicketBelongsToUserDepartment($ticket, $fromCoworker, $message)
+    {
+        if (!in_array($ticket->support_department_id, $fromCoworker->supportDepartments->pluck('id')->toArray())) {
+            return __('messages.tickets.' . $message);
+        }
     }
 
     public function assignToDepartment($request)
@@ -129,17 +141,17 @@ class TicketService
 
         $fromCoworker = auth()->guard('coworkers')->user();
 
-        if (!in_array($ticket->support_department_id, $fromCoworker->supportDepartments->pluck('id')->toArray())) {
-            return __('messages.tickets.you_cant_assign_another_department_ticket');
-        }
+        $result = $this->isTicketBelongsToUserDepartment($ticket, $fromCoworker, 'you_cant_assign_another_department_ticket');
+
+        if (is_string($result)) return $result;
 
         if ($ticket->support_department_id == $request->input('support_department_id')) {
             return __('messages.tickets.ticket_belongs_to_this_department');
         }
 
-        if (is_null($ticket->opened_by) || $ticket->opened_by != $fromCoworker->id) {
-            return __('messages.tickets.you_cant_assign_the_ticket_which_you_dont_open');
-        }
+        $result = $this->isTicketOpenedByCurrentUser($ticket, $fromCoworker, 'you_cant_assign_the_ticket_which_you_dont_open');
+
+        if (is_string($result)) return $result;
 
         $ticket->update([
             'is_opened' => 0,
@@ -161,9 +173,9 @@ class TicketService
             $q->where('name', Role::SUPER_ADMIN->value);
         })->get();
 
-        foreach ($admins as $admin) {
+        $admins->each(function ($admin) use ($ticket, $fromCoworker) {
             $admin->notify(new AssignTicketToDepartmentNotification($ticket->supportDepartment, $fromCoworker));
-        }
+        });
 
         return true;
     }
--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::group(['prefix' => '/coworkers', 'middleware' => 'auth:coworkers'], funct
 Route::post('/roles/{role}/assign-permission', [RolePermissionController::class, 'assignPermissionToRole'])->middleware('auth:sanctum');
 
 Route::group(['prefix' => '/panel'], function () {
-    Route::apiResource('/tickets', TicketController::class);
+    Route::apiResource('/tickets', TicketController::class)->middleware('auth');
     Route::post('/tickets/exists/{userId}/{departmentId}', [TicketController::class, 'userTicketHistoryCheck']);
 
     Route::apiResource('support-tickets', SupportTicketController::class)->middleware('auth:coworkers');
