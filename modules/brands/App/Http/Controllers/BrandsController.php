fbd246ed96b976430e65b6b2d77458dc3f9327cd
Amir Tavakolian <rezatva@gmail.com>
2025-03-28 14:04:58 +0330

    feat: brands index

--- modules/brands/App/Http/Controllers/BrandsController.php
+++ modules/brands/App/Http/Controllers/BrandsController.php
@@ -1,8 +1,19 @@
 <?php
 
+namespace Brands\App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
+use App\Services\ApiResponse\ApiResponseFacade;
+use Brands\App\Http\Resources\BrandsResource;
+use Brands\App\Models\Brand;
 
 class BrandsController extends Controller
 {
 
+    public function index()
+    {
+        $brands = Brand::all();
+
+        return ApiResponseFacade::setData(BrandsResource::collection($brands))->build()->response();
+    }
 }
--- modules/brands/Routes/brand_routes.php
+++ modules/brands/Routes/brand_routes.php
@@ -1,4 +1,9 @@
 <?php
 
+use Brands\App\Http\Controllers\BrandsController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => '/panel', 'middleware' => 'auth:coworkers'], function () {
+
+    Route::apiResource('/brands', BrandsController::class);
+});
